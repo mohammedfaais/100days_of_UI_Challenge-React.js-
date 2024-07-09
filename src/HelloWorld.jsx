865d5672b440c9e962eb9a7d06073a2ec82ecf77
Faais <81349206+mohammedfaais@users.noreply.github.com>
2024-07-09 09:44:29 +0000

    Day1

--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function HelloWorld() {
-  return <h1>Hello World From individual component </h1>;
+  return <h1>Hello World From individual component</h1>;
 }
 
 export default HelloWorld;
